a6bddb235d57ef0b1d971b759c788f5afabfccd1
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2023-11-12 16:46:30 +0100

    feat: create custom error handling middleware

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads/public')));
 
+app.use((req, res) => {
+  res.status(404).send('The requested URL was not found');
+});
+
+const isDebugMode = process.execArgv.some((arg) => arg.includes('--inspect'));
+app.use((err, req, res, next) => {
+  logger.error(err);
+  if (process.env.NODE_ENV !== 'production') {
+    if (isDebugMode) console.error(err);
+    res.status(err.isOperational ? err.code : 500).json(err);
+  } else if (err.isOperational) {
+    res.status(err.code).json(err);
+  } else {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
