66dceceec0cae900e04b9f4c8eaa21df34d98eb6
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2023-11-12 15:01:08 +0100

    install necessary dependencies

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const http = require('http');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({ origin: [process.env.APP_DOMAIN, process.env.ADMIN_APP_DOMAIN] }));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads/public')));
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
