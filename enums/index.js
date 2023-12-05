068bff60e994007d6b245772d5f35623a0b5cf94
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2023-12-05 15:50:20 +0100

    feat: Add CATEGORY_TYPE enum and Subcategory model

---
+++ enums/index.js
@@ -0,0 +1,6 @@
+
+exports.CATEGORY_TYPE = {
+    DESIGN: 'Design',
+    MARKETING_DIGITAL: 'Digital Marketing',
+    DEVELOPMENT: 'Development',
+};
---
+++ models/Subcategory.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { CATEGORY_TYPE } = require('../enums');
+
+const subcategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: Object.values(CATEGORY_TYPE),
+      required: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+const Subcategory = mongoose.model('Subcategory', subcategorySchema);
+
+module.exports = Subcategory;
